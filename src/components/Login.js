58a974f948b3edab15f81a93191e93134458dbd3
Alok Kumar <alokk13799@gmail.com>
2023-02-14 13:07:05 +0530

    Login page uncontrolled component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,54 @@
 import React from "react";
 
-export default function Login(props) {
-	console.log(props);
-	return (
-		<div>
-			<h1>This is Login page component</h1>
-			<h3>Say Hello and welcome</h3>
-		</div>
-	);
+class Login extends React.Component {
+	// uncontrolled form component
+	constructor(props) {
+		super(props);
+		this.emailInputRef = React.createRef();
+		this.passwordInputRef = React.createRef();
+	}
+
+	// event for handle form submit
+	handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log("emailInputRef ", this.emailInputRef);
+		console.log("passwordInputRef ", this.passwordInputRef);
+	};
+
+	render() {
+		return (
+			<div>
+				<form className="login-form">
+					<span className="login-signup-header">Log In</span>
+					<div className="field">
+						<input
+							type="text"
+							placeholder="Email"
+							ref={this.emailInputRef}
+							required
+							/* Part of uncontrolled component method */
+							// ref={this.emailInputRef}
+						/>
+					</div>
+					<div className="field">
+						<input
+							type="password"
+							placeholder="*******"
+							ref={this.passwordInputRef}
+							required
+							/* Part of uncontrolled component method */
+							// ref={this.passwordInputRef}
+						/>
+					</div>
+					<div className="field">
+						<button type="submit" onClick={(e) => this.handleFormSubmit(e)}>
+							Log In
+						</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
+
+export default Login;
